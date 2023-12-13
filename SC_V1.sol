fb24fbc3b96114136fa691bfd5e9fc90231da439
Clément <clab60917@eleve.isep.fr>
2023-12-13 22:31:50 +0100

    V0.2_final

--- SC_V1.sol
+++ SC_V1.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract LuxuryWatchNFT is ERC721Enumerable, Ownable {
-    using Counters for Counters.Counter;
-    using Strings for uint256;
-    Counters.Counter private _tokenIds;
+contract LuxuryWatch is ERC721, Ownable {
+    constructor(address initialOwner)
+        ERC721("LuxuryWatch", "MTK")
+        Ownable(initialOwner)
+    {}
+}
 
     struct Watch {
         string brand;
@@ -18,12 +20,21 @@ contract LuxuryWatchNFT is ERC721Enumerable, Ownable {
         uint256 price;
     }
 
-    uint256 public constant royaltyPercentage = 5; // 5% royalty
+    uint256 public constant royaltyPercentage = 5;
     mapping(uint256 => Watch) public watchInfo;
     mapping(uint256 => address[]) public watchOwnershipHistory;
     mapping(uint256 => string) public watchMetadataURI;
 
-    // Events
+    // Définition des erreurs personnalisées
+    error Unauthorized(address caller);
+    error PriceTooLow(uint256 requestedPrice, uint256 currentPrice);
+    error TokenNotExist(uint256 tokenId);
+    error NotOwner(address owner, address caller);
+    error InsufficientFunds(uint256 available, uint256 required);
+    error NotTokenOwner(address caller, uint256 tokenId);
+
+
+    // Événements
     event WatchNFTCreated(uint256 indexed tokenId, string name, string model, string serialNumber);
     event WatchNFTSold(uint256 indexed tokenId, address from, address to, uint256 price);
     event ApprovalForSale(uint256 indexed tokenId, address indexed approved, uint256 price);
@@ -46,28 +57,27 @@ contract LuxuryWatchNFT is ERC721Enumerable, Ownable {
         watchOwnershipHistory[newItemId].push(msg.sender);
         watchMetadataURI[newItemId] = _metadataURI;
 
-        emit WatchNFTCreated(newItemId, _brand, _model, _serialNumber);
+        emit WatchNFTCreated(newItemId, _name, _model, _serialNumber);
     }
 
     function approveForSale(uint256 _tokenId, address _approved, uint256 _price) public {
-        require(ownerOf(_tokenId) == msg.sender, "Only the owner can approve for sale");
+        if (ownerOf(_tokenId) != msg.sender) revert NotOwner(ownerOf(_tokenId), msg.sender);
         approve(_approved, _tokenId);
         watchInfo[_tokenId].price = _price;
         emit ApprovalForSale(_tokenId, _approved, _price);
     }
 
     function sellWatchNFT(uint256 _tokenId, address _buyer) public {
-        require(getApproved(_tokenId) == _buyer, "The buyer is not approved by the owner");
+        if (getApproved(_tokenId) != _buyer) revert Unauthorized(msg.sender);
         uint256 salePrice = watchInfo[_tokenId].price;
         uint256 royaltyFee = (salePrice * royaltyPercentage) / 100;
 
-        // Transfer the royalty fee to the original creator
         address payable creator = payable(ownerOf(_tokenId));
-        require(payable(creator).send(royaltyFee), "Could not pay out royalties");
+        if (address(this).balance < royaltyFee) revert InsufficientFunds(address(this).balance, royaltyFee);
+        if (!creator.send(royaltyFee)) revert PriceTooLow(salePrice, royaltyFee);
 
-        // Transfer the remaining amount to the seller
         address payable seller = payable(msg.sender);
-        require(payable(seller).send(salePrice - royaltyFee), "Could not pay out seller");
+        if (!seller.send(salePrice - royaltyFee)) revert InsufficientFunds(address(this).balance, salePrice - royaltyFee);
 
         watchOwnershipHistory[_tokenId].push(_buyer);
         _transfer(msg.sender, _buyer, _tokenId);
@@ -75,17 +85,19 @@ contract LuxuryWatchNFT is ERC721Enumerable, Ownable {
         emit WatchNFTSold(_tokenId, msg.sender, _buyer, salePrice);
     }
 
-    function getWatchOwnershipHistory(uint256 _tokenId) public view returns (address[] memory) {
+      function getWatchOwnershipHistory(uint256 _tokenId) public view returns (address[] memory) {
         return watchOwnershipHistory[_tokenId];
     }
 
     function updateMetadataURI(uint256 _tokenId, string memory _newMetadataURI) public {
-        require(ownerOf(_tokenId) == msg.sender, "Only the owner can update the metadata");
-        watchMetadataURI[_tokenId] = _newMetadataURI;
+    if (ownerOf(_tokenId) != msg.sender) revert NotTokenOwner(msg.sender, _tokenId);
+    watchMetadataURI[_tokenId] = _newMetadataURI;
+    
     }
 
+
     // On peut mettre ici d'autres fonctions si besoin
-}
+
 
 //fonction approveForSale permet au propriétaire d'un NFT de donner l'approbation à un autre utilisateur pour vendre la montre NFT.
 //fonction sellWatchNFT a été modifiée pour inclure la logique de paiement des royalties au créateur original du NFT.
@@ -95,3 +107,6 @@ contract LuxuryWatchNFT is ERC721Enumerable, Ownable {
 // RÉSUMÉ GÉNÉRAL DU CODE :
 //Ce code est un point de départ qui crée un token NFT pour chaque montre avec des détails comme le nom, le modèle, le numéro de série pour la traçabilité, et le prix.
 //Il comprend des événements pour la création et la vente.
+
+    // ...
+}
