36cb5479f6e6fb7214e9ec748bfbade4f4a72c1e
Clément <clab60917@eleve.isep.fr>
2023-12-17 17:25:17 +0100

    truffle compile working !

--- contracts/SC_V1.sol
+++ contracts/SC_V1.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract LuxuryWatch is ERC721, Ownable {
 
